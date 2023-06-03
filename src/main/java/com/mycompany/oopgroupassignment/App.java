7b5feceeeec0d55400dec8c8e5b2644ef7d327fb
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2023-06-03 21:46:42 +0800

    Added image

--- src/main/java/com/mycompany/oopgroupassignment/App.java
+++ src/main/java/com/mycompany/oopgroupassignment/App.java
@@ -1,30 +1,53 @@
 package com.mycompany.oopgroupassignment;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 /**
- * JavaFX App
+ * MOHAMED LUQMAN HADI BIN MOHAMED SUHAIRI 2210855
+ * Object-Oriented Programming Group Project
  */
+
+//reminder: use array for new order and customer class objects
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+    public void start(Stage stage) throws FileNotFoundException {
+        //Image import and ImageView for main program logo
+        Image orderScrn = new Image(new FileInputStream("main.png"));
+        ImageView orderScrnView = new ImageView(orderScrn); 
+        //hbox for displaying food item from food class
+        
+        
+        
+        
+        
+        //vbox for all food-related hboxes
+        
+        
+        var scene = new Scene(new StackPane(orderScrnView), 500, 500);
         stage.setScene(scene);
         stage.show();
     }
 
+    
+    
+    
+    
     public static void main(String[] args) {
         launch();
+    }
+    public void makeFile(){
+        
     }
 
 }
