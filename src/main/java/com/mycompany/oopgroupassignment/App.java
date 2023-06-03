283e1f4f8a988d84eb4c10231954aa67392e6fb0
Luqman Hadi <102597758+luqhardy@users.noreply.github.com>
2023-06-03 22:40:21 +0800

    Can add fooditems

--- src/main/java/com/mycompany/oopgroupassignment/App.java
+++ src/main/java/com/mycompany/oopgroupassignment/App.java
@@ -14,10 +14,25 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
 
 
 /**
@@ -35,17 +50,19 @@ public class App extends Application {
         Image orderScrn = new Image(new FileInputStream("main.png"));
         ImageView orderScrnView = new ImageView(orderScrn); 
         //tableview for displaying food item from food class
-        TableView foodItems = new TableView();
-        TableColumn col1 = new TableColumn("Image");
-        TableColumn col2 = new TableColumn("Name");
-        TableColumn col3 = new TableColumn("Price");
+        TableView foodItems = new TableView<>();
+        TableColumn<Food,String> col1 = new TableColumn<>("Image");
+        col1.setCellValueFactory(new PropertyValueFactory<>("Image"));
+        TableColumn<Food,String> col2 = new TableColumn<>("Name");
+        col2.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        TableColumn<Food,Double> col3 = new TableColumn<>("Price");
         TableColumn col4 = new TableColumn("Quantity");
         TableColumn col5 = new TableColumn("Add/Remove");
         
         foodItems.getColumns().addAll(col1,col2,col3,col4,col5);
         
         //array for 10 food items demo
-        
+        foodItems.getItems().add(new Food("Burger","Burger",9.99));
         
         
         //HBox for Total Price and checkout button
